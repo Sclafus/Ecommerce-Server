72bab1315189fc49d328ffb15ea436ac1a06d1d4
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-11-27 21:53:14 +0100

    spacing

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.SocketException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
@@ -21,9 +20,7 @@ public class ServerThread extends Thread {
 			while ((message = bufferReader.readLine()) != null) {
 				PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
 				printWriter.println("server received: " + message);
-
 			}
-			
 			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
