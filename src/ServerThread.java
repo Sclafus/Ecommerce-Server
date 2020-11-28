0bebf3f271fbf197ef2519ce082f1ea1eaea2ef6
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-11-28 22:18:15 +0100

    query executor, it works with for now

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -6,6 +6,9 @@ import java.io.PrintWriter;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 public class ServerThread extends Thread {
 	private Socket socket;
 
@@ -18,9 +21,16 @@ public class ServerThread extends Thread {
 		try {
 			String message = null;
 			BufferedReader bufferReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			Connection db = getConnection();
-
+			//reads from client input
 			while ((message = bufferReader.readLine()) != null) {
+				for(Object[] b : execQuery("SELECT * FROM assignment3.user", "id", "name", "surname")){
+					System.out.println("[");
+					for(Object c : b){
+						System.out.print((String) c);
+					}
+					System.out.println("]");
+				}
+				//server -> client
 				PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
 				System.out.println(message);
 				printWriter.println("server received: " + message);
@@ -35,13 +45,13 @@ public class ServerThread extends Thread {
 	public static Connection getConnection(){
 		
 		try{
-			//username and password are in clear, who cares tbh
+			//username and password are in clear, who cares tbh, just for testing purposes
 			String driver = "com.mysql.cj.jdbc.Driver";
 			String url = "jdbc:mysql://localhost:3306/assignment3";
 			String username = "root";
 			String password = "admin";
 			Class.forName(driver);
-		
+
 			Connection connection = DriverManager.getConnection(url, username, password);
 			System.out.println("database connected!");
 			return connection;
@@ -51,4 +61,33 @@ public class ServerThread extends Thread {
 		return null;
 	}	
 
+	public static ArrayList<Object[]> execQuery(String query, String... fields) {
+		try {
+			//connects to db, executes the query, gets result
+			Connection connection = getConnection();
+			PreparedStatement statement = connection.prepareStatement(query);
+			ResultSet query_result = statement.executeQuery();
+
+			ArrayList<Object> tmp = new ArrayList<Object>();
+			ArrayList<Object[]> result = new ArrayList<Object[]>();
+			/* 
+			checks every element of result, inserts it in tmp,
+			tmp gets converted to an array of strings and then cleared,
+
+			*/
+			while (query_result.next()){
+				for (String field : fields){
+					tmp.add(query_result.getString(field));
+				}
+				Object[] tuple_array = tmp.toArray();
+				result.add(tuple_array);
+				tmp.clear();
+			}
+			return result;
+
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
