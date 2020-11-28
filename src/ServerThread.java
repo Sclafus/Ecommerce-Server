46526d336ea27bb7e3746784f8443a0a93cbacd0
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-11-28 19:22:32 +0100

    database connection using jdbc and mysql

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -4,6 +4,8 @@ import java.net.Socket;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 public class ServerThread extends Thread {
 	private Socket socket;
 
@@ -16,9 +18,11 @@ public class ServerThread extends Thread {
 		try {
 			String message = null;
 			BufferedReader bufferReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			Connection db = getConnection();
 
 			while ((message = bufferReader.readLine()) != null) {
 				PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+				System.out.println(message);
 				printWriter.println("server received: " + message);
 			}
 			socket.close();
@@ -27,4 +31,24 @@ public class ServerThread extends Thread {
 		}
 
 	}
+
+	public static Connection getConnection(){
+		
+		try{
+			//username and password are in clear, who cares tbh
+			String driver = "com.mysql.cj.jdbc.Driver";
+			String url = "jdbc:mysql://localhost:3306/assignment3";
+			String username = "root";
+			String password = "admin";
+			Class.forName(driver);
+		
+			Connection connection = DriverManager.getConnection(url, username, password);
+			System.out.println("database connected!");
+			return connection;
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}	
+
 }
