177e4438d50b4d385cf2dba30cd15e993ff91edf
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-11-30 14:09:36 +0100

    added login, updated db structure (supports notifications)

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -1,14 +1,18 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import java.util.ArrayList;
+
 public class ServerThread extends Thread {
 	private Socket socket;
 
@@ -19,33 +23,34 @@ public class ServerThread extends Thread {
 	public void run() {
 
 		try {
-			String message = null;
-			BufferedReader bufferReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			//reads from client input
-			while ((message = bufferReader.readLine()) != null) {
-				for(Object[] b : execQuery("SELECT * FROM assignment3.user", "id", "name", "surname")){
-					System.out.println("[");
-					for(Object c : b){
-						System.out.print((String) c);
-					}
-					System.out.println("]");
+			InputStream inputStream = this.socket.getInputStream();
+			ObjectInputStream in = new ObjectInputStream(inputStream);
+			try {
+				String[] message = (String[]) in.readObject();
+				OutputStream outputStream = this.socket.getOutputStream();
+				ObjectOutputStream out = new ObjectOutputStream(outputStream);
+				// stuff happens here!
+				switch (message[0]) {
+					case "login":
+					Boolean login_result = login(message[1], message[2]);
+					out.writeObject(login_result);
+						break;
+
+					default:
+						break;
 				}
-				//server -> client
-				PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-				System.out.println(message);
-				printWriter.println("server received: " + message);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
 			}
-			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
-	public static Connection getConnection(){
-		
-		try{
-			//username and password are in clear, who cares tbh, just for testing purposes
+	public static Connection getConnection() {
+
+		try {
+			// username and password are in clear, who cares tbh, just for testing purposes
 			String driver = "com.mysql.cj.jdbc.Driver";
 			String url = "jdbc:mysql://localhost:3306/assignment3";
 			String username = "root";
@@ -53,13 +58,36 @@ public class ServerThread extends Thread {
 			Class.forName(driver);
 
 			Connection connection = DriverManager.getConnection(url, username, password);
-			System.out.println("database connected!");
 			return connection;
-		} catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
-	}	
+	}
+
+	public static Boolean login(String username, String password) {
+		Connection connection = getConnection();
+		String query = String.format("SELECT email, password FROM user WHERE email='%s'", username);
+		try {
+			PreparedStatement statement = connection.prepareStatement(query);
+			ResultSet query_result = statement.executeQuery();
+
+			if(!query_result.next()){
+				return false;
+			} else {
+				String pwd = query_result.getString("password");
+				
+				if (password.equals(pwd)){
+					return true;
+				} else {
+					return false;
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	public static ArrayList<Object[]> execQuery(String query, String... fields) {
 		try {
@@ -68,22 +96,27 @@ public class ServerThread extends Thread {
 			PreparedStatement statement = connection.prepareStatement(query);
 			ResultSet query_result = statement.executeQuery();
 
-			ArrayList<Object> tmp = new ArrayList<Object>();
 			ArrayList<Object[]> result = new ArrayList<Object[]>();
-			/* 
-			checks every element of result, inserts it in tmp,
-			tmp gets converted to an array of strings and then cleared,
 
-			*/
-			while (query_result.next()){
-				for (String field : fields){
-					tmp.add(query_result.getString(field));
+			if(fields.length > 0){
+				ArrayList<Object> tmp = new ArrayList<Object>();
+				/* 
+				checks every element of result, inserts it in tmp,
+				tmp gets converted to an array of strings and then cleared,
+	
+				*/
+				while (query_result.next()){
+					for (String field : fields){
+						tmp.add(query_result.getString(field));
+					}
+					Object[] tuple_array = tmp.toArray();
+					result.add(tuple_array);
+					tmp.clear();
 				}
-				Object[] tuple_array = tmp.toArray();
-				result.add(tuple_array);
-				tmp.clear();
+				return result;
+			} else {
+
 			}
-			return result;
 
 		} catch (Exception e){
 			e.printStackTrace();
